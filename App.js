18fd465b20a9d6755c27855cf8d18735b04aae9c
AliHamza0007 <141808379+AliHamza0007@users.noreply.github.com>
2024-06-13 11:14:25 +0500

    intial comits

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View} from 'react-native';
+import Functional from './components/Functional';
+import Class from './components/Class';
+import UseEffect from './components/UseEffect';
+import Responsiveui from './components/Responsiveui';
+import Button from './components/Button';
+import Radio from './components/Radio';
+import DynamicRadio from './components/DynamicRadio';
+import Modal from './components/Modal';
+import Pressable from './components/Pressable';
+
+const App = () => {
+  return (
+    <View>
+      {/* <Functional /> */}
+      {/* <Class /> */}
+      {/* <UseEffect /> */}
+      {/* <Responsiveui /> */}
+      {/* <Button /> */}
+      {/* <Radio /> */}
+      {/* <DynamicRadio /> */}
+      <Modal />
+      {/* <Pressable /> */}
+    </View>
+  );
+};
+
+export default App;
